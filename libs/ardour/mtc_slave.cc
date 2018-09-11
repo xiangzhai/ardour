19a245a5ab57fc060354cdb06ef340f0193ed205
Paul Davis <paul@linuxaudiosystems.com>
2018-09-11 12:05:44 -0400

    ensure that MTC-driven locates actually work

--- libs/ardour/mtc_slave.cc
+++ libs/ardour/mtc_slave.cc
@@ -455,8 +455,8 @@ MTC_TransportMaster::update_mtc_time (const MIDI::byte *msg, bool was_full, samp
 	if (was_full || outside_window (mtc_frame)) {
 		DEBUG_TRACE (DEBUG::MTC, string_compose ("update_mtc_time: full TC %1 or outside window %2 MTC %3\n", was_full, outside_window (mtc_frame), mtc_frame));
 		_session->set_requested_return_sample (-1);
-		_session->request_transport_speed (0);
-		_session->request_locate (mtc_frame, false);
+		_session->request_transport_speed (0, TRS_MTC);
+		_session->request_locate (mtc_frame, false, TRS_MTC);
 		update_mtc_status (MIDI::MTC_Stopped);
 		reset (false);
 		reset_window (mtc_frame);
